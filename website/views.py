d09bc8f448feaf0d1858d09056341feae76d161b
Hamza Madi <166062319+lionhamza@users.noreply.github.com>
2025-06-22 00:14:28 +0200

    Profile Image layout
    Profile Image layout for the user page

--- website/views.py
+++ website/views.py
@@ -1,7 +1,18 @@
 from flask import Blueprint,render_template
 views=Blueprint('views',__name__)
 
-@views.route('/')
-def home():
-    return render_template("home.html")
 
+@views.route('/')
+def Base():
+  return render_template("home.html")
+
+
+@views.route('/profile')
+def profile():
+    user = {
+        "name": "Hamza Madi",
+        "address": "Physical Address",
+        "profession": "Plumber",
+        "experience": "Expert"
+    }
+    return render_template("userProfile.html", user=user)
