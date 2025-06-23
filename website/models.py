cac48c1f4818eb95a2d0849c383be0534847b2c2
Hamza Madi <166062319+lionhamza@users.noreply.github.com>
2025-06-24 00:45:41 +0200

    Edit Profile
    Added functionality into edit profile Button...next stop work on apploading Profile and Cover Pictures

--- website/models.py
+++ website/models.py
@@ -5,12 +5,14 @@ class User(db.Model):
 
     ID = db.Column(db.Integer, primary_key=True)
     Name = db.Column(db.String)
+    Email=db.Column(db.String)
     Location = db.Column(db.String)
     Service = db.Column(db.String)
-    #experience = db.Column(db.String) 
+    Experience = db.Column(db.String) 
     CellPhone = db.Column(db.String)
-    #availability = db.Column(db.String)
-    #rating = db.Column(db.Float)
-    #reviews = db.Column(db.Integer)
-
-    #image = db.Column(db.String)  # e.g. 'assets/electrician.jpg'
+    availability = db.Column(db.String)
+    Rating = db.Column(db.Float)
+    Reviews = db.Column(db.Integer)
+    Bio =db.Column(db.String)
+    Surname=db.Column(db.String)
+    Image = db.Column(db.String)  # e.g. 'assets/electrician.jpg'
--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .models import User
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -11,3 +12,19 @@ def Base():
 def profile(user_id):
     user = User.query.get_or_404(user_id)
     return render_template("userProfile.html", user=user)
+
+@views.route('/profile/<int:user_id>/update', methods=['POST'])
+def update_profile(user_id):
+    user = User.query.get_or_404(user_id)
+
+    user.Bio = request.form['bio']
+    user.Location = request.form['location']
+    user.Experience = request.form['experience']
+    user.CellPhone = request.form['cellphone']
+    user.Email = request.form['email']
+    user.availability = request.form['availability']
+
+    db.session.commit()
+
+    flash('Profile successfully updated!', 'success')  # <-- Flash message
+    return redirect(url_for('views.profile', user_id=user_id))
