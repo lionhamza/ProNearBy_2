980a9ca08b9eb81057d5280b7020d3248be1f082
Hamza Madi <166062319+lionhamza@users.noreply.github.com>
2026-09-18 02:36:11 +0200

    Update venv packages and website templates
    Apply numerous updates to the virtualenv site-packages (pip, click, sqlalchemy, greenlet, itsdangerous, jinja2, pip vendors, etc.), adjust blinker RECORD entries for Python 3.13 pyc filenames, and export additional symbols from click (__init__.py) such as ParameterSource, custom_version_option and NoSuchCommand. Also update venv activation scripts/pyvenv.cfg and make minor UI changes to website static/pro_signup_mobile.css and registration templates (register_certified.html, register_experienced.html). These changes reflect package upgrades and small frontend tweaks.

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\MTC-ADM\Documents\GitHub\ProNearBy_2\venv"
+set "VIRTUAL_ENV=C:\Users\Student\Documents\GitHub\ProNearBy_1\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
